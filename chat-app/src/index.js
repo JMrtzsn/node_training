e680c93a677068234e22b73d2854e5b6d2f9e70b
Johannes Mauritzson <johannes.mauritzson@gmail.com>
2020-05-02 09:11:28 +0200

    Start working on chat-app: realtime coms testing

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,8 +1,14 @@
-const app = require("../app")
+const express = require("express")
+const path = require("path")
+const app = express()
+
 const port = process.env.PORT || 3000
 
+const dirpath = path.join(__dirname, "../public")
+app.use(express.json())
+
 app.get("", (req, res) => {
-    res.render("index")
+    res.sendFile(path.join(dirpath + "/index.html"))
 })
 
 app.listen(port, () => {
